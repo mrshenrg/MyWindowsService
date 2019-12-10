11d831be5aa18fd96baf3c2f887fb03159eeefc6
mrshenrg <darhen@163.com>
2019-12-10 19:30:45 +0800

    Timer 定时执行

--- MyWindowsService/MyService.cs
+++ MyWindowsService/MyService.cs
@@ -6,6 +6,9 @@ namespace MyWindowsService
 {
     public partial class MyService : ServiceBase
     {
+        System.Timers.Timer _Timer;  //计时器
+        private static object _LockSMS_Send = new object();
+
         public MyService()
         {
             InitializeComponent();
@@ -25,6 +28,13 @@ namespace MyWindowsService
                 writer.WriteLine(string.Format("{0},服务启动！", DateTime.Now));
                 
             }
+
+            // TODO: 在此处添加代码以启动服务。
+            int minute = 1; // Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["GapDate"].ToString());
+            this._Timer = new System.Timers.Timer();
+            this._Timer.Interval = minute * 60 * 1000;  //设置计时器事件间隔执行时间
+            this._Timer.Elapsed += new System.Timers.ElapsedEventHandler(Timer_Elapsed);
+            this._Timer.Enabled = true;
         }
 
         protected override void OnStop()
@@ -54,6 +64,25 @@ namespace MyWindowsService
         {
             //系统即将关闭执行代码
             base.OnShutdown();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //Log.Read(DateTime.Now.ToString("-----------------------------------时 间：yyyy-MM-dd HH:mm:ss------------------------------------"));
+            //DateTime t1 = DateTime.Now;
+            //Log.Read(" ");
+            //Log.Read(string.Format("执行时间：{0}分{1}秒", (DateTime.Now - t1).Minutes, (DateTime.Now - t1).Seconds));
+            //Log.Read(" ");
+
+            lock(_LockSMS_Send)
+            {
+                using (FileStream stream = new FileStream(filePath, FileMode.Append))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    //writer.WriteLine("{DateTime.Now},执行时间！");
+                    writer.WriteLine(string.Format("{0},Timer执行时间！", DateTime.Now));
+                }
+            }
         }
     }
 }
--- MyWindowsService/ProjectInstaller.cs
+++ MyWindowsService/ProjectInstaller.cs
@@ -14,6 +14,16 @@ namespace MyWindowsService
         public ProjectInstaller()
         {
             InitializeComponent();
+
+            //在安装服务后一般还需手动启动，添加如下代码实现安装后自动启动
+            this.Committed += new InstallEventHandler(ProjectInstaller_Committed);
         }
+
+        private void ProjectInstaller_Committed(object sender, InstallEventArgs e)
+        {
+            //参数为服务的名字
+            System.ServiceProcess.ServiceController controller = new System.ServiceProcess.ServiceController("MyService");
+            controller.Start();
+        } 
     }
 }
