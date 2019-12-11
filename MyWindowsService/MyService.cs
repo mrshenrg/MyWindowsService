8b6c6b55cbb3eafaabb1695df7983e11310fa06f
mrshenrg <darhen@163.com>
2019-12-11 14:16:24 +0800

    记录到event log中，在[事件查看器]可查询

--- MyWindowsService/MyService.cs
+++ MyWindowsService/MyService.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.ServiceProcess;
 using System.Diagnostics;
+using System.Configuration;
+using System.Threading;
 using System.IO;
 
 namespace MyWindowsService
 {
     public partial class MyService : ServiceBase
     {
-        //记录到event log中，地址是 C:\Windows\System32\winevt\Logs (双击查看即可，文件名为MyNewLog)
+        //记录到event log中，在[事件查看器]可查询，地址是 C:\WINDOWS\system32\config\MyNewLog.evt (双击查看即可，文件名为MyNewLog)
         private static System.Diagnostics.EventLog eventLog1;
-        private static object _LockSMS_Send = new object();
-
-        //System.Timers.Timer _Timer;  //计时器
         private int eventId = 1;
+
+        System.Timers.Timer _Timer;  //计时器
+        private static object _LockSMS_Send = new object();
 
         public MyService()
         {
@@ -28,21 +30,22 @@ namespace MyWindowsService
         }
 
         //"@" 让转移字符"\"保持原意，不要转义
-        static string filePath = @"C:\MyServiceLog.txt"; // "C:\\MyServiceLog.txt";
+        static string filePath = @"C:\MyServiceLog.txt"; // == "C:\\MyServiceLog.txt";
 
         protected override void OnStart(string[] args)
         {
+            //服务开启执行代码
             eventLog1.WriteEntry("In OnStart.");
             log("In OnStart.");
-            //服务开启执行代码
 
             // TODO: 在此处添加代码以启动服务。
-            int minute = 1; // Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["GapDate"].ToString());
-            System.Timers.Timer _Timer = new System.Timers.Timer();
-            //this._Timer = new System.Timers.Timer();
-            _Timer.Interval = minute * 60 * 1000;  //设置计时器事件间隔执行时间
-            _Timer.Elapsed += new System.Timers.ElapsedEventHandler(Timer_Elapsed);
-            _Timer.Enabled = true;
+            int second = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["GapDate"].ToString());
+            if (second == 0) { second = 5; }
+
+            this._Timer = new System.Timers.Timer();
+            this._Timer.Interval = second * 1000;  //minute * 60 * 1000; 设置计时器事件间隔执行时间
+            this._Timer.Elapsed += new System.Timers.ElapsedEventHandler(Timer_Elapsed);
+            this._Timer.Enabled = true;
         }
 
         protected override void OnStop()
@@ -71,7 +74,7 @@ namespace MyWindowsService
         }
 
         /// <summary>
-        /// 记录到指定路径：D:\log.txt
+        /// 记录到指定路径：C:\MyServiceLog.txt
         /// </summary>
         /// <param name="message"></param>
         private static void log(string message)
@@ -86,17 +89,22 @@ namespace MyWindowsService
 
         private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            //Log.Read(DateTime.Now.ToString("-----------------------------------时 间：yyyy-MM-dd HH:mm:ss------------------------------------"));
-            //DateTime t1 = DateTime.Now;
-            //Log.Read(" ");
-            //Log.Read(string.Format("执行时间：{0}分{1}秒", (DateTime.Now - t1).Minutes, (DateTime.Now - t1).Seconds));
-            //Log.Read(" ");
-
             eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
             lock(_LockSMS_Send)
             {
-                log("Timer执行时间！");
+                DateTime t1 = DateTime.Now;
+                Thread.Sleep(3000);
+                log(string.Format("Timer执行时间：{0}分{1}秒", (DateTime.Now - t1).Minutes, (DateTime.Now - t1).Seconds));
             }
+
+            System.Configuration.ConfigurationManager.RefreshSection("appSettings");
+            int second = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["GapDate"].ToString());
+            if (second != 0 && second * 1000 != this._Timer.Interval) 
+            {
+                log(second.ToString());
+                this._Timer.Interval = second * 1000;  //设置计时器事件间隔执行时间
+            }
+            
         }
     }
 }
